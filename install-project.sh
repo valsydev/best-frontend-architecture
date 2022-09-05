b6df1718f3f623c2b18e0d34f8c884f6ae838f01
Vladyslav Tamashchuk <30079677+valsydev@users.noreply.github.com>
2022-09-05 18:33:37 +0300

    Update install-project.sh

--- install-project.sh
+++ install-project.sh
@@ -3,3 +3,4 @@ echo 'You should put the same project name twice ( $projectname )'
 npx create-next-app@latest --typescript
 git clone git@github.com:valsydev/best-frontend-architecture.git
 cp -rf ./best-frontend-architecture/src ./projectname
+rm -rf ./best-frontend-architecture
