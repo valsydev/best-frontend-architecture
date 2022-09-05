449ea97d7bf2a8358cbe06e3626be631b6081d60
Vladyslav Tamashchuk <30079677+valsydev@users.noreply.github.com>
2022-09-05 18:38:24 +0300

    Update install-project.sh

--- install-project.sh
+++ install-project.sh
@@ -1,5 +1,5 @@
 read -p 'Project Name in format "my-best-project": ' projectname
-echo You should put the same project name twice ( $projectname )
+echo You should put the same project name twice - $projectname
 npx create-next-app@latest --typescript
 git clone git@github.com:valsydev/best-frontend-architecture.git
 cp -rf ./best-frontend-architecture/src ./$projectname
